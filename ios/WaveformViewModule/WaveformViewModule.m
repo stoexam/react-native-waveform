e42a1853c1866525262a2f113b466ee68ce06a5c
leeshare <leexuliang@163.com>
2017-07-12 11:46:28 +0800

    ios iflytec pcm to mp3

--- ios/WaveformViewModule/WaveformViewModule.m
+++ ios/WaveformViewModule/WaveformViewModule.m
@@ -14,16 +14,17 @@
 #import <React/RCTEventDispatcher.h>
 
 #import "ISEParams.h"
-#import "PopupView.h"
+#import "tools/PopupView.h"
 #import "iflyMSC/IFlyMSC.h"
 
 #import <objc/runtime.h>
 
-#import "ISEResult.h"
-#import "ISEResultXmlParser.h"
+#import "Results/ISEResult.h"
+#import "Results/ISEResultXmlParser.h"
 #import "Definition.h"
 
 #import "WaveformDialog.h"
+#import "lame.h"
 
 #pragma mark - const values
 
@@ -114,7 +115,7 @@ RCT_EXPORT_METHOD(alert:(NSString *)message){
     //[self.iFlySpeechEvaluator setParameter:@"json" forKey:[IFlySpeechConstant ISE_RESULT_TYPE]];
     
     //[self.iFlySpeechEvaluator setParameter:@"eva.pcm" forKey:[IFlySpeechConstant ISE_AUDIO_PATH]];
-    [self.iFlySpeechEvaluator setParameter:@"self.mp3" forKey:[IFlySpeechConstant ISE_AUDIO_PATH]];
+    [self.iFlySpeechEvaluator setParameter:@"self.pcm" forKey:[IFlySpeechConstant ISE_AUDIO_PATH]];
     
     NSStringEncoding encoding = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingGB_18030_2000);
     
@@ -401,6 +402,55 @@ RCT_EXPORT_METHOD(isWaveformShow:
     
 }
 
+- (void)audio_PCMtoMP3
+{
+    NSString *sourcePCM = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"self.pcm"];
+    
+     NSString *saveToMp3 = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"self.mp3"];
+    
+    @try {
+        int read, write;
+        
+        FILE *pcm = fopen([sourcePCM cStringUsingEncoding:1], "rb");  //source 被转换的音频文件位置
+        fseek(pcm, 4*1024, SEEK_CUR);                                   //skip file header
+        FILE *mp3 = fopen([saveToMp3 cStringUsingEncoding:1], "wb");  //output 输出生成的Mp3文件位置
+        
+        const int PCM_SIZE = 20000;//8192
+        const int MP3_SIZE = 20000;//8192
+        short int pcm_buffer[PCM_SIZE*2];
+        unsigned char mp3_buffer[MP3_SIZE];
+        
+        lame_t lame = lame_init();
+        lame_set_in_samplerate(lame, 11025.0);
+        lame_set_VBR(lame, vbr_default);
+        lame_init_params(lame);
+        
+        do {
+            read = fread(pcm_buffer, 2*sizeof(short int), PCM_SIZE, pcm);
+            if (read == 0)
+            {
+                write = lame_encode_flush(lame, mp3_buffer, MP3_SIZE);
+            }
+            else
+                write = lame_encode_buffer_interleaved(lame, pcm_buffer, read, mp3_buffer, MP3_SIZE);
+            
+            fwrite(mp3_buffer, write, 1, mp3);
+            
+        } while (read != 0);
+        
+        lame_close(lame);
+        fclose(mp3);
+        fclose(pcm);
+    }
+    @catch (NSException *exception) {
+        NSLog(@"%@",[exception description]);
+    }
+    @finally {
+        //do some
+    }
+    
+}
+
 /*!
  *  停止录音回调
  *    当调用了`stopListening`函数或者引擎内部自动检测到断点，如果没有发生错误则回调此函数。
@@ -411,7 +461,8 @@ RCT_EXPORT_METHOD(isWaveformShow:
     if ([self.iseParams.audioSource isEqualToString:IFLY_AUDIO_SOURCE_STREAM]){
         [_pcmRecorder stop];
     }
-    
+    //转换为mp3
+    [self audio_PCMtoMP3];
 }
 
 /*!
